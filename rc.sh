7fba8e61e87b1fe0076ccf7853f21821031e4599
Miguel Martinez <slverwolf@gmail.com>
2018-02-08 10:12:52 -0600

    build: Add recompile script
    For rerunning builds without cleaning

---
+++ rc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+############################################################
+### Single run script for HolyDragon Kernel
+############################################################
+
+# This is a cleanup and single run build script for testing purposes.
+# This does not zip anything for you, only executes a normal build. 
+
+# Minimum requirements to build:
+# Already working build environment :P 
+#
+# In this script: 
+# You will need to change the 'Source Path to kernel tree' to match your current path to this source.
+# You will need to change the 'Compile Path to out' to match your current path to this source.
+# You will also need to edit the '-j32' under 'Start Compile' section and adjust that to match the amount of cores you want to use to build.
+# 
+# In Makefile: 
+# You will need to edit the 'CROSS_COMPILE=' line to match your current path to this source.
+# 
+# Once those are done, you should be able to execute './run.sh' from terminal and receive an Image.gz-dtb file in arch/arm64/boot/
+
+############################################################
+# Build Script Variables
+############################################################ 
+
+# Source defconfig used to build
+	dc=HDO_defconfig
+
+# Source Path to kernel tree
+	k=/home/holyangel/android/msm8996
+
+# Source Path to clean(empty) out folder
+	co=$k/out
+
+# Compile Path to out 
+	o="O=/home/holyangel/android/msm8996/out"
+
+############################################################
+# Start Compile
+############################################################
+
+	echo "	Starting compile.."
+	make "$o" -j32
+	echo "	First pass completed!"
+	echo "	"
