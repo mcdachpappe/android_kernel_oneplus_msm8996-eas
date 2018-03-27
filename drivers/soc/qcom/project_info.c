fa5609870133261dd8e798fe70270c7373942df2
Miguel Martinez <slverwolf@gmail.com>
2018-03-27 13:55:12 -0500

    Fix last uninitialized warnings

--- drivers/soc/qcom/project_info.c
+++ drivers/soc/qcom/project_info.c
@@ -405,7 +405,7 @@ uint32 get_hw_version(void)
 int __init init_project_info(void)
 {
 	static bool project_info_init_done;
-	int ddr_size;
+	int ddr_size = 0;
 
 	if (project_info_init_done)
 		return 0;
