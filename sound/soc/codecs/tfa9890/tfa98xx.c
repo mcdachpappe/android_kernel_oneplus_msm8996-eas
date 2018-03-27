03f945e563469e90a6e1a729a2459ce1e91d3222
Miguel Martinez <slverwolf@gmail.com>
2018-03-27 10:43:56 -0500

    tfa9890: Change debug warnings to errors.

--- sound/soc/codecs/tfa9890/tfa98xx.c
+++ sound/soc/codecs/tfa9890/tfa98xx.c
@@ -72,7 +72,7 @@ EXPORT_SYMBOL_GPL(tfa_codec_np);
 static ssize_t tfa98xx_state_store(struct device *dev, struct device_attribute *attr,
 		const char *buf, size_t count)
 {
-	pr_debug("%s",__func__);
+    pr_err("%s",__func__);
 
 	return 0;
 }
@@ -408,7 +408,7 @@ static int tfa98xx_digital_mute(struct snd_soc_dai *dai, int mute)
 	struct snd_soc_codec *codec = dai->codec;
 	struct tfa98xx *tfa98xx = snd_soc_codec_get_drvdata(codec);
 
-	pr_debug("state: %d\n", mute);
+	pr_err("state: %d\n", mute);
 
 	mutex_lock(&tfa98xx->dsp_init_lock);
 
@@ -465,7 +465,7 @@ static int tfa98xx_trigger(struct snd_pcm_substream *substream, int cmd,
 	//struct tfa98xx *tfa98xx = snd_soc_codec_get_drvdata(codec);
 	int ret = 0;
 
-	pr_debug("trigger:%d(0:stop 1:start)\n", cmd);
+	pr_err("trigger:%d(0:stop 1:start)\n", cmd);
 
 	switch (cmd) {
 	case SNDRV_PCM_TRIGGER_START:
@@ -701,7 +701,7 @@ void tfa98xx_play_stop(void)
 {
 	struct tfa98xx *tfa98xx = g_tfa98xx;
 
-    pr_debug("tfa stop\n");
+    pr_err("tfa stop\n");
 
     if(g_tfa98xx == NULL)
     {
