1583085bc76cd9bb61de323207c3afea96e71a65
Christian S <crian84@gmail.com>
2018-06-19 01:36:58 +0200

    input: synaptics: s1302: make disable buttons on touch configurable
    All credits to andip71 <andreasp@gmx.de>
    
    Sysfs: /sys/module/synaptics_driver_s1302/parameters/no_buttons_during_touch Y(default)/N
    
    Signed-off-by: crian <crian84@gmail.com>
    Signed-off-by: danny aeng <noreference@web.de>

--- drivers/input/touchscreen/synaptics_driver_s1302.c
+++ drivers/input/touchscreen/synaptics_driver_s1302.c
@@ -56,6 +56,9 @@
 #include "synaptics_s1302_redremote.h"
 //#include <linux/boot_mode.h>
 #include <linux/project_info.h>
+
+#include <linux/moduleparam.h>
+
 enum oem_boot_mode{
 	MSM_BOOT_MODE__NORMAL,
 	MSM_BOOT_MODE__FASTBOOT,
@@ -91,6 +94,9 @@ enum oem_boot_mode{
 		pr_err(LOG_TAG ": " a,##arg);\
 	}while(0)
 
+//module parameter
+bool no_buttons_during_touch = 1;
+module_param(no_buttons_during_touch, bool, 0644);
 
 //#define SUPPORT_FOR_COVER_ESD
 #define SUPPORT_VIRTUAL_KEY
@@ -2076,7 +2082,10 @@ bool s1302_is_keypad_stopped(void)
 {
 	struct synaptics_ts_data *ts = tc_g;
 
-	return ts ? ts->stop_keypad : false;
+	if (no_buttons_during_touch)
+		return ts->stop_keypad;
+
+	return false;
 }
 
 static void synaptics_input_event(struct input_handle *handle,
@@ -2087,8 +2096,11 @@ static void synaptics_input_event(struct input_handle *handle,
 	if (code != BTN_TOOL_FINGER)
 		return;
 
-	/* Disable capacitive keys when user's finger is on touchscreen */
-	ts->stop_keypad = value;
+	if (no_buttons_during_touch)
+		/* Disable capacitive keys when user's finger is on touchscreen */
+		ts->stop_keypad = value;
+	else
+		ts->stop_keypad = false;
 }
 
 static int synaptics_input_connect(struct input_handler *handler,
