2e7ff47f5bed7178b78a74309141edd0c5b87032
Sultanxda <sultanxda@gmail.com>
2018-08-22 20:40:01 +0100

    msm: mdss: Don't constantly fire interrupts for DSI status check
    The DSI status check that uses the vsync GPIO results in an interrupt
    being fired every time a vsync event occurs, which results in several
    interrupts being fired and processed constantly for no reason.
    
    Instead of needlessly letting the vsync interrupt fire non-stop, force it
    to fire at most twice every STATUS_CHECK_INTERVAL_MS (5000 ms). This
    results in reduced CPU usage for this feature.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>
    Signed-off-by: danny aeng <noreference@web.de>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1672,9 +1672,9 @@ static int mdss_dsi_unblank(struct mdss_panel_data *pdata)
         mutex_unlock(&ctrl_pdata->panel_mode_lock);
     }
 	if ((pdata->panel_info.type == MIPI_CMD_PANEL) &&
-		mipi->vsync_enable && mipi->hw_vsync_mode) {
+		mipi->vsync_enable && mipi->hw_vsync_mode)
 		mdss_dsi_set_tear_on(ctrl_pdata);
-	}
+
 	ctrl_pdata->ctrl_state |= CTRL_STATE_PANEL_INIT;
 
 error:
@@ -1741,9 +1741,8 @@ static int mdss_dsi_blank(struct mdss_panel_data *pdata, int power_state)
 	}
 
 	if ((pdata->panel_info.type == MIPI_CMD_PANEL) &&
-		mipi->vsync_enable && mipi->hw_vsync_mode) {
+		mipi->vsync_enable && mipi->hw_vsync_mode)
 		mdss_dsi_set_tear_off(ctrl_pdata);
-	}
 
 	if (ctrl_pdata->ctrl_state & CTRL_STATE_PANEL_INIT) {
 		if (!pdata->panel_info.dynamic_switch_pending) {
@@ -3390,7 +3389,7 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 		init_completion(&ctrl_pdata->te_irq_comp);
 		rc = devm_request_irq(&pdev->dev,
 			gpio_to_irq(ctrl_pdata->disp_te_gpio),
-			hw_vsync_handler, IRQF_TRIGGER_FALLING,
+			hw_vsync_handler, IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
 			"VSYNC_GPIO", ctrl_pdata);
 		if (rc) {
 			pr_err("%s: TE request_irq failed for ESD\n", __func__);
