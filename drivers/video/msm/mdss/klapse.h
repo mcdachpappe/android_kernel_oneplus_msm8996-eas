1df808865ad479a2459ec3d1bfff9bddfbe2e362
tanish2k09 <tanish2k09.dev@gmail.com>
2019-03-23 16:25:50 -0700

    K-lapse initial implementation for MDSS kcal
    Signed-off-by: danny aeng <noreference@web.de>

--- drivers/video/msm/mdss/klapse.h
+++ drivers/video/msm/mdss/klapse.h
@@ -1,11 +1,31 @@
 #ifndef _LINUX_KLAPSE_H
 #define _LINUX_KLAPSE_H
 
-/* Required variables for external access. Change as per use */
+/* KLAPSE_MDSS : Use 1 if using with MDSS */
+#define KLAPSE_MDSS 1
+
+/* set_rgb_slider : Interface function for brightness-mode */
 extern void set_rgb_slider(u32 bl_lvl);
 
-// from mdss kcal
-extern void kcal_klapse_push(int r, int g, int b);
+/* This file uses generalised K_### defines
+ * The interpretation should be the respective color's var
+ */
+#define K_TYPE   unsigned short
+
+#if KLAPSE_MDSS
+ #define K_RED    kcal_get_color(0)
+ #define K_GREEN  kcal_get_color(1)
+ #define K_BLUE   kcal_get_color(2)
+
+extern K_TYPE kcal_get_color(unsigned short int code);
+extern void klapse_kcal_push(int r, int g, int b);
+#else
+ #define K_RED    kcal_red
+ #define K_GREEN  kcal_green
+ #define K_BLUE   kcal_blue
+
+ extern K_TYPE K_RED, K_GREEN, K_BLUE;
+#endif
 
 /* DEFAULT_ENABLE values :
  * 0 = off
@@ -21,19 +41,19 @@ extern void kcal_klapse_push(int r, int g, int b);
 #define SCALE_VAL_MIN   20
 
 // MAX_BRIGHTNESS : Maximum value of the display brightness/backlight
-#define MAX_BRIGHTNESS  255
+#define MAX_BRIGHTNESS  1023
 
 // MIN_BRIGHTNESS : Minimum value of the display brightness/backlight
-#define MIN_BRIGHTNESS  5
+#define MIN_BRIGHTNESS  2
 
-/* UPPER_BL_LVL : Initial upper limit for brightness-dependent mode.
+/* UPPER_BL_LVL : Initial upper limit for brightness-dependent mode. 
  * Value <= MAX_BRIGHTNESS && > LOWER_BL_LVL (MUST)
  */
-#define UPPER_BL_LVL  255
+#define UPPER_BL_LVL  200
 
-/* LOWER_BL_LVL : Initial lower limit for brightness-dependent mode.
+/* LOWER_BL_LVL : Initial lower limit for brightness-dependent mode. 
  * Value < UPPER_BL_LVL (MUST)
  */
-#define LOWER_BL_LVL 5
+#define LOWER_BL_LVL 2
 
 #endif  /* _LINUX_KLAPSE_H */
